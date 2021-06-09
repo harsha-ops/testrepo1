0439955107f36ea92395ab940a3e95c401140ef6
Jason McElvain <jasonom2@yahoo.com>
2021-06-09 11:10:45 -0500

    python project

---
+++ pythonExamApp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path("", views.index),
+    path("register", views.register),
+    path("travels", views.travels),
+    path("logout", views.logout),
+    path("login", views.login),
+    path("travels/add", views.addtrip),
+    path("createtrip", views.createtrip),
+    path("destination/<tripID>", views.tripdetails),
+    path("JoinTrip/<tripID>", views.JoinTrip),
+    path("cancel/<tripID>", views.cancel)
+]
+ 
